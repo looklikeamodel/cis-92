4381a2b1e5c0735a2774e464c5c04f3dd9ab614e
looklikeamodel <kabylbayevyerkezhan@gmail.com>
2024-04-18 21:31:09 +0000

    csrfupdated(github.dev)

--- mysite/mysite/settings.py
+++ mysite/mysite/settings.py
@@ -29,7 +29,7 @@ DEBUG = bool(os.environ.get('DEBUG', True))
 ALLOWED_HOSTS = [ "*" ]
 
 # Trust all origins for CSRF validation
-CSRF_TRUSTED_ORIGINS = ['https://*.cloudshell.dev', 'https://*.github.dev.com']
+CSRF_TRUSTED_ORIGINS = ['https://*.cloudshell.dev', 'https://*.github.dev']
 
 
 # Application definition
